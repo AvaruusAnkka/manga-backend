ec541cd3ed34911bc1b21466441a833ed0db2ce5
Andy <andy0ph@gmail.com>
2024-11-11 15:56:25 +0200

    request for chapter urls and manga struct on separate file

--- src/mangadex/mangaDto.go
+++ src/mangadex/mangaDto.go
@@ -46,24 +46,6 @@ type en struct {
 	EN string `json:"en"`
 }
 
-type Manga struct {
-	ID          string   `json:"id"`
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Chapters    int      `json:"chapters"`
-	CoverUrl    string   `json:"coverUrl"`
-	Created     string   `json:"created"`
-	Status      string   `json:"status"`
-	Tags        []string `json:"tags"`
-	Updated     string   `json:"updated"`
-}
-
-type Chapter struct {
-	Id        string   `json:"id"`
-	Number    int      `json:"number"`
-	ImageUrls []string `json:"imageUrls"`
-}
-
 type AggregateResponse struct {
 	Result  string `json:"result"`
 	Volumes map[string]struct {
