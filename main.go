99fe2eff2a33b05bc4cd7d533ab9ae9077fe3ddd
Andy <andy0ph@gmail.com>
2024-11-12 19:50:35 +0200

    ready for hosting

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/AvaruusAnkka/manga-backend/src/mangadex"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
 
 	router.GET("/manga", mangadex.GetManga)
 
 	router.GET("/chapter", mangadex.GetChapter)
 
-	router.Run("localhost:8080")
+	router.Run(fmt.Sprintf(":%s", port))
 }
