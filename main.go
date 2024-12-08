df23a018aa6898ecdda8c50047e9b36532d07b13
Andy <60817686+AvaruusAnkka@users.noreply.github.com>
2024-12-08 23:17:44 +0200

    Gin (#3)
    * ready for hosting
    
    * enabled cors

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/AvaruusAnkka/manga-backend/src/mangadex"
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := gin.Default()
+	router.Use(CORSMiddleware())
+
+	router.SetTrustedProxies(nil)
 
 	router.GET("/manga", mangadex.GetManga)
 
 	router.GET("/chapter", mangadex.GetChapter)
 
-	router.Run("localhost:8080")
+	router.Run(fmt.Sprintf(":%s", port))
 }
